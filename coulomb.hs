6016db4c7e48e7b6a04b1e4332443ab0644cda23
Andrew Chen <awc8888@gmail.com>
2016-05-27 23:31:38 -0700

    basic fns def

--- coulomb.hs
+++ coulomb.hs
@@ -1,6 +1,29 @@
+import System.IO
+import System.Environment
+
 k = 8.987e9
 
-force :: (Integral q1, Integral q2, Integral dist) => q1 -> q2 -> dist -> Int
-force q1 q2 dist = (k * q1 * q2) / (dist^2)
+--force :: Double -> Double -> Double -> Double
+--force q1 q2 dist = (k * q1 * q2) / (distance^2)
 
-dist :: (Integral )
+distance :: (Double, Double) -> (Double, Double) -> Double
+distance a b = sqrt((snd b - snd a)^2 + (fst b - fst a)^2)
+
+-- returns a difference vector (x, y) with direction a -> b
+diffVec :: (Double, Double) -> (Double, Double) -> (Double, Double)
+diffVec a b = (fst b - fst a, snd b - snd a)
+
+-- returns a unit vector (x, y) with direction a -> b
+unitVec :: (Double, Double) -> (Double, Double) -> (Double, Double)
+unitVec a b = mapTuple f (diffVec a b)
+    where f c = c / (distance a b)
+
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
+mapTuple f (a1, a2) = (f a1, f a2)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    if length args < 1
+        then putStrLn "First argument should be the path to the input file."
+        else putStrLn $ args !! 0
